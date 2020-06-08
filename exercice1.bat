6e10c57ae7b4846dd13e87a2efd5805e15dff6ec
Méloé <meloscavezzoni@gmail.com>
2020-06-08 12:10:13 +0200

    Exercice 1 : set up

--- exercice1.bat
+++ exercice1.bat
@@ -6,3 +6,14 @@ REM       the number is larger or smaller
 REM    4. Repeat the last two steps until the user secceds
 
 
+@echo off
+title exercice1
+:: Size
+mode con cols=80 lines=16
+:: Color
+color 90
+
+:: CODE
+
+
+pause > nul
