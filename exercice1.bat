16767a8e5943fe4a528e9d9b72fd2159098946be
Méloé <meloscavezzoni@gmail.com>
2020-06-08 12:08:17 +0200

    Exercice 1 : program statement

---
+++ exercice1.bat
@@ -0,0 +1,8 @@
+REM The program : 
+REM    1. The computer choose a random number
+REM    2. We ask the user to choose a number
+REM    3. The computer compares the two numbers and indicates whether
+REM       the number is larger or smaller
+REM    4. Repeat the last two steps until the user secceds
+
+
