a059765db556a7a7d14c1f6f9802ca36f8a082d5
Méloé <meloscavezzoni@gmail.com>
2020-06-10 11:39:55 +0200

    Exercise instructions

---
+++ exercice2.bat
@@ -0,0 +1,6 @@
+REM Improvement of exercice 1
+REM     1. Do 10 difficulty levels: the higher the level, the higher 
+REM                                 the number chosen by the computer.
+REM     2. At the end, the user chooses to start again or to quit.
+REM     3. Tell how many tries the user has succeeded.
+REM     4. Save score to file.
