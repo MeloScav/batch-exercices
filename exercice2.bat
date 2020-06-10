6294ec91a3cb1402f33bb421f17b06e32e6dafbb
Méloé <meloscavezzoni@gmail.com>
2020-06-10 21:52:19 +0200

    Removal of instructions

--- exercice2.bat
+++ exercice2.bat
@@ -1,10 +1,3 @@
-REM Improvement of exercice 1
-REM     1. Do 10 difficulty levels: the higher the level, the higher 
-REM                                 the number chosen by the computer.
-REM     2. At the end, the user chooses to start again or to quit.
-REM     3. Tell how many tries the user has succeeded.
-REM     4. Save score to file.
-
 @echo off
 title exercice2
 :: Size
