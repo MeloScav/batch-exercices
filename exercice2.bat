ea9f01abf60383406e8ef25d4ec348223155ee15
Méloé <meloscavezzoni@gmail.com>
2020-06-10 20:40:39 +0200

    Exercice 2 : difficulty level

--- exercice2.bat
+++ exercice2.bat
@@ -19,6 +19,21 @@ set minNbr = 1
 set maxNbr = 10
 set nbrOfTries = 0
 
+:: The user chooses a difficulty level
+:difficulty
+    set /p level=Choose a difficulty level between 1 and 10 :
+    :: The maximum number changes depending on the level chosen
+    if %level% == 1 set maxNbr = 10
+    if %level% == 2 set maxNbr = 100
+    if %level% == 3 set maxNbr = 500
+    if %level% == 4 set maxNbr = 1000
+    if %level% == 5 set maxNbr = 2000
+    if %level% == 6 set maxNbr = 3000
+    if %level% == 7 set maxNbr = 4000
+    if %level% == 8 set maxNbr = 5000
+    if %level% == 9 set maxNbr = 6000
+    if %level% == 10 set maxNbr = 7000
+
 :: The computer choose a random number
 :random_number
     set randomNbr=%RANDOM%
