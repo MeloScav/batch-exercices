c25555c07351ccb264bb4e33e7266826dc1819bc
Méloé <meloscavezzoni@gmail.com>
2020-06-10 23:17:21 +0200

    Exercice 2 : corrections

--- exercice2.bat
+++ exercice2.bat
@@ -14,18 +14,18 @@ set nbrOfTries=0
 
 :: The user chooses a difficulty level
 :difficulty
-    set /p level=Choose a difficulty level between 1 and 10 :
+    set /p level=Choose a difficulty level between 1 and 10 : 
     :: The maximum number changes depending on the level chosen
-    if %level% == 1 set maxNbr = 10
-    if %level% == 2 set maxNbr = 100
-    if %level% == 3 set maxNbr = 500
-    if %level% == 4 set maxNbr = 1000
-    if %level% == 5 set maxNbr = 2000
-    if %level% == 6 set maxNbr = 3000
-    if %level% == 7 set maxNbr = 4000
-    if %level% == 8 set maxNbr = 5000
-    if %level% == 9 set maxNbr = 6000
-    if %level% == 10 set maxNbr = 7000
+    if %level% == 1 set maxNbr=10
+    if %level% == 2 set maxNbr=100
+    if %level% == 3 set maxNbr=500
+    if %level% == 4 set maxNbr=1000
+    if %level% == 5 set maxNbr=2000
+    if %level% == 6 set maxNbr=3000
+    if %level% == 7 set maxNbr=4000
+    if %level% == 8 set maxNbr=5000
+    if %level% == 9 set maxNbr=6000
+    if %level% == 10 set maxNbr=7000
 
 :: The computer choose a random number
 :random_number
@@ -37,9 +37,9 @@ set nbrOfTries=0
 
 :: The user choose a number
 :start
-    set /p userNbr =Choose a number : 
+    set /p userNbr=Choose a number : 
     :: Increasement number of trials by 1
-    set /a nbrOfTries = %nbrOfTries% + 1 
+    set /a nbrOfTries=%nbrOfTries% + 1 
     :: If the chosen number is < than the random number, we launch greater
     if %userNbr% LSS %randomNbr% goto greater
     :: If the chosen number is > than the random number, we launch lower
@@ -58,13 +58,12 @@ set nbrOfTries=0
 :end
     echo Good game ! You succeeded in %nbrOfTries% tries 
     :: If the file doesn't exist, we create it
-    If not exist scores.txt ( echo Difficulty level : %level% )
+    If not exist scores.txt ( echo Difficulty level : %level% > scores.txt )
     :: Add in the file end
     echo Number of tries : %nbrOfTries% >> scores.txt
 
-    set /p finalDecision = What do you want to do ? 1. Restart 2. Exit
-    if %finalDecision%==1 ( goto difficulty ) else( exit )
-
-
+    set /p finalDecision=What do you want to do ? 1. Restart 2. Exit 
+    echo.
+    if %finalDecision%==1 ( goto difficulty ) else ( exit )
 
 pause > nul
